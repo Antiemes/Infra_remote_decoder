bbe1cc3c4d3cd4a848df155f1fff2f8c02fdc64e
Helguli <helguli@yahoo.com>
2021-02-16 17:35:17 +0100

    Dekódolás

--- src/decoder.ino
+++ src/decoder.ino
@@ -2,52 +2,62 @@
 
 void pinChange()
 {
-	static volatile uint32_t t0=0;
-  uint8_t pulseLevel=digitalRead(RX_PIN);
-  uint32_t tNow=micros();
-	uint32_t pulseLength=tNow-t0;
-	t0=tNow;
-  //Serial.print(pulseLength);
-  //Serial.print(" ");
-  //Serial.print(pulseLevel);
-  //Serial.print("\n");
-	if (pulseLevel==0)
-	{
-		if (pulseLength>300 && pulseLength<600)
-		{
-  		Serial.print("0");
+    static volatile uint32_t t0=0;
+    uint8_t pulseLevel=digitalRead(RX_PIN);
+    uint32_t tNow=micros();
+    uint32_t pulseLength=tNow-t0;
+    t0=tNow;
+    //Serial.print(pulseLength);
+    //Serial.print(" ");
+    //Serial.print(pulseLevel);
+    //Serial.print("\n");
+    if (pulseLevel==0)
+    {
+        if (pulseLength>300 && pulseLength<600)
+//        if (pulseLength>700 && pulseLength<1200) //szurke
+        {
+            Serial.print("0");
 		}
-		else if (pulseLength>1200 && pulseLength<2000)
-		{
-  		Serial.print("1");
-		}
-		else
-		{
-  		//Serial.print("Rx ");
-  		//Serial.println(pulseLength);
-  		Serial.print("\n");
-
-		}
-	}
-	else
-	{
-		if (pulseLength>500 && pulseLength<800)
-		{
-		}
-		else
-		{
-  		//Serial.print("Ry ");
-  		//Serial.println(pulseLength);
-		}
-	}
+        else if (pulseLength>1200 && pulseLength<2000)
+//        else if (pulseLength>1600 && pulseLength<2000) //szurke
+        {
+            Serial.print("1");
+        }
+        else
+        {
+//            Serial.print("Rx ");
+//            Serial.println(pulseLength);
+            Serial.print("\n");
+        }
+    }
+    else
+    {
+        if (pulseLength>500 && pulseLength<800)
+//        if (pulseLength>700 && pulseLength<1200) //szurke
+        {
+//            Serial.print("1");
+            //Serial.print("Ry ");
+            //Serial.println(pulseLength);
+        }
+        /*
+        else if (pulseLength>1600 && pulseLength<2000) //szurke
+        {
+            Serial.print("11");
+        }*/
+        else
+        {
+//            Serial.print("Ry ");
+//            Serial.println(pulseLength);
+        }
+    }
 
 }
 
 void setup()
 {
-  Serial.begin(115200);
-	pinMode(RX_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(RX_PIN), pinChange, CHANGE);
+    Serial.begin(115200);
+    pinMode(RX_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(RX_PIN), pinChange, CHANGE);
 }
 
 void loop()
