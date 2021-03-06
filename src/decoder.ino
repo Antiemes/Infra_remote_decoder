8d74bcd723a03aec1d5b3ef1e472fbcb2dfb1255
Helguli <helguli@yahoo.com>
2021-03-06 19:20:38 +0100

    Codes recognised, only the number of the button printed

--- src/decoder.ino
+++ src/decoder.ino
@@ -1,4 +1,18 @@
 #define RX_PIN 2
+#define N_OF_BUTTONS 21
+
+uint32_t code = 0;
+uint16_t remote_codes[] =
+{0b1010001001011101, 0b0110001010011101, 0b1110001000011101,
+0b0010001011011101, 0b0000001011111101, 0b1100001000111101,
+0b1110000000011111, 0b1010100001010111, 0b1001000001101111,
+0b0110100010010111, 0b1001100001100111, 0b1011000001001111,
+0b0011000011001111, 0b0001100011100111, 0b0111101010000101,
+0b0001000011101111, 0b0011100011000111, 0b0101101010100101,
+0b0100001010111101, 0b0100101010110101, 0b0101001010101101};
+uint16_t detected = 0;
+
+
 
 void pinChange()
 {
@@ -16,18 +30,23 @@ void pinChange()
         if (pulseLength>300 && pulseLength<600)
 //        if (pulseLength>700 && pulseLength<1200) //szurke
         {
-            Serial.print("0");
+            code = code << 1;
+//            Serial.print("0");
 		}
         else if (pulseLength>1200 && pulseLength<2000)
 //        else if (pulseLength>1600 && pulseLength<2000) //szurke
         {
-            Serial.print("1");
+            code = (code << 1) + 1;
+//            Serial.print("1");
         }
         else
         {
 //            Serial.print("Rx ");
 //            Serial.println(pulseLength);
-            Serial.print("\n");
+//            Serial.print("\n");
+//            Serial.println(code, HEX);
+            detected = code & 0xFFFF;
+            code = 0;
         }
     }
     else
@@ -79,4 +98,12 @@ void loop()
   //  }
   //  Serial.print("\n");
   //}
+    for (int i = 0; i < N_OF_BUTTONS; i++)
+    {
+        if (detected == remote_codes[i])
+        {
+            Serial.println(i);
+            detected = 0;
+        }
+    }
 }
