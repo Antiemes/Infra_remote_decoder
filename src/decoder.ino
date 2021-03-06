9d87a019dbd819c8caad09da9112c826f7fdadc1
Helguli <helguli@yahoo.com>
2021-03-06 22:07:30 +0100

    Pin rename & deleting unecessary code

--- src/decoder.ino
+++ src/decoder.ino
@@ -1,8 +1,8 @@
-#define RX_PIN 2
+#define IR_PIN 2
 #define N_OF_BUTTONS 21
 
 uint32_t code = 0;
-uint16_t remote_codes[] =
+uint16_t remoteCodes[] =
 {0b1010001001011101, 0b0110001010011101, 0b1110001000011101,
 0b0010001011011101, 0b0000001011111101, 0b1100001000111101,
 0b1110000000011111, 0b1010100001010111, 0b1001000001101111,
@@ -16,34 +16,27 @@ uint16_t detected = 0;
 
 void pinChange()
 {
-    static volatile uint32_t t0=0;
-    uint8_t pulseLevel=digitalRead(RX_PIN);
-    uint32_t tNow=micros();
-    uint32_t pulseLength=tNow-t0;
-    t0=tNow;
-    //Serial.print(pulseLength);
-    //Serial.print(" ");
-    //Serial.print(pulseLevel);
-    //Serial.print("\n");
-    if (pulseLevel==0)
+    static volatile uint32_t t0 = 0;
+    uint8_t pulseLevel = digitalRead(IR_PIN);
+    uint32_t tNow = micros();
+    uint32_t pulseLength = tNow - t0;
+    t0 = tNow;
+    if (pulseLevel == 0)
     {
-        if (pulseLength>300 && pulseLength<600)
-//        if (pulseLength>700 && pulseLength<1200) //szurke
+        if (pulseLength > 300 && pulseLength < 600)
+//        if (pulseLength > 700 && pulseLength < 1200) //szurke
         {
             code = code << 1;
 //            Serial.print("0");
 		}
-        else if (pulseLength>1200 && pulseLength<2000)
-//        else if (pulseLength>1600 && pulseLength<2000) //szurke
+        else if (pulseLength > 1200 && pulseLength < 2000)
+//        else if (pulseLength > 1600 && pulseLength < 2000) //szurke
         {
             code = (code << 1) + 1;
 //            Serial.print("1");
         }
         else
         {
-//            Serial.print("Rx ");
-//            Serial.println(pulseLength);
-//            Serial.print("\n");
 //            Serial.println(code, HEX);
             detected = code & 0xFFFF;
             code = 0;
@@ -51,23 +44,18 @@ void pinChange()
     }
     else
     {
-        if (pulseLength>500 && pulseLength<800)
-//        if (pulseLength>700 && pulseLength<1200) //szurke
+        if (pulseLength > 500 && pulseLength < 800)
+//        if (pulseLength > 700 && pulseLength < 1200) //szurke
         {
 //            Serial.print("1");
             //Serial.print("Ry ");
             //Serial.println(pulseLength);
         }
         /*
-        else if (pulseLength>1600 && pulseLength<2000) //szurke
+        else if (pulseLength > 1600 && pulseLength < 2000) //szurke
         {
             Serial.print("11");
         }*/
-        else
-        {
-//            Serial.print("Ry ");
-//            Serial.println(pulseLength);
-        }
     }
 
 }
@@ -75,32 +63,15 @@ void pinChange()
 void setup()
 {
     Serial.begin(115200);
-    pinMode(RX_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(RX_PIN), pinChange, CHANGE);
+    pinMode(IR_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(IR_PIN), pinChange, CHANGE);
 }
 
 void loop()
 {
-//  uint8_t i;
-//  Serial.print("Foo\n");
-//  delay(500);
-
-  //delay(100);
-  //if (rxPackets)
-  //{
-  //  Serial.print("(");
-  //  Serial.print(rxPackets);
-  //  rxPackets=0;
-  //  Serial.print(") ");
-  //  for (i=0; i<30; i++)
-  //  {
-  //    Serial.print(packet[i]);
-  //  }
-  //  Serial.print("\n");
-  //}
     for (int i = 0; i < N_OF_BUTTONS; i++)
     {
-        if (detected == remote_codes[i])
+        if (detected == remoteCodes[i])
         {
             Serial.println(i);
             detected = 0;
